a2f940c5073950ae32eb594a363d6d6606ee3f2f
Mark Bradley <braddle@gmail.com>
2013-02-24 14:42:02 +0000

    Use autoloader.

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 
-require_once 'Tweet.php';
-require_once 'TwitterSearcher.php';
-require_once 'TwitterWordCount.php';
-require_once 'WordCounter.php';
-require_once 'JsonResponse.php';
+
+function __autoload($class_name)
+{
+    include_once $class_name . '.php';
+}
 
 TwitterWordCount::run();
